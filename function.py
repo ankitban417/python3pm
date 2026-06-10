c6bac770e0bac1bd370b97e25394cabda2ff5b7e
ankitban417 <ankitban417@gmail.com>
2026-06-10 14:37:02 +0545

    add file*

---
+++ function.py
@@ -0,0 +1,38 @@
+# def add(a,b):
+#     print(a+b)
+# add(5,10)
+
+# def product(a,b):
+#     print(a*b)
+
+# product(5,10)
+
+# def sub():
+#     x=int(input("enter first number:"))
+#     y=int(input("enter second number"))
+#     subtract=x-y
+#     return subtract
+
+# print(sub())
+
+# def table():
+#     num=int(input("enter a number of which you want multiplication table"))
+#     a=1
+#     while a<=10:
+#         print(f"{num}*{a}=",num*a)
+#         a+=1
+# table()
+
+# def checkEvenorOdd(x):
+    
+#     if x%2==0:
+#         print("the number is even")
+#     else:
+#         print("the number is odd")
+# checkEvenorOdd(1)
+
+def add(a,b):
+    print(a+b)
+add(5,10)
+
+
